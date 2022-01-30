6176068c14d716def16f8bb81ed188a0c777a53f
MishaBlin <wildline007@gmai.com>
2022-01-30 23:24:32 +0500

    6

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html', style_href=url_for('static', filename='styles/style.css'))

5dd6f738e8decc92547e53522c28cd77830419a9
MishaBlin <wildline007@gmai.com>
2022-01-30 22:20:22 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html', style_href=url_for('static', filename='styles/style.css'))
+
+
+@app.route('/2.html')
+def food():
+    return render_template('2.html', style_href=url_for('static', filename='styles/style2.css'))
+
+
+@app.route('/3.html')
+def breeds():
+    return render_template('3.html', style_href=url_for('static', filename='styles/style3.css'))
+
+
+@app.route('/4.html')
+def games():
+    return render_template('4.html', style_href=url_for('static', filename='styles/style4.css'))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
